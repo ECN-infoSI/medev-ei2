d857e3c9f7c22802089829281d1e16bb7436db33
truaux <thomas003.ruaux@gmail.com>
2024-11-14 11:43:43 +0100

    update

--- jeu/src/main/java/org/centrale/medev/monopoly/Paiement.java
+++ jeu/src/main/java/org/centrale/medev/monopoly/Paiement.java
@@ -7,18 +7,13 @@ public class Paiement extends Evenement {
     public Joueur joueur ;
     
     public Paiement(String nom, int position, int prix){
-        super(position,nom) ;
+        super(nom, position) ;
         this.prix = prix ;
     }
 
     public Paiement(){
         super() ;
         this.prix = 2000 ;
-    }
-    
-    public Paiement(Paiement p){
-        super(p);
-        this.prix = p.prix ;
     }
     
     public void effet(){
@@ -29,25 +24,8 @@ public class Paiement extends Evenement {
         this.prix = prix;
     }
 
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
     public int getPrix() {
         return prix;
     }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-    
     
 }
