3cb2b9fa2315bbb6ff2176e6288622fba2223621
Victor SIMON <victor.simon760@gmail.com>
2024-11-14 10:10:11 +0100

    toString method

--- jeu/src/main/java/org/centrale/medev/monopoly/Case.java
+++ jeu/src/main/java/org/centrale/medev/monopoly/Case.java
@@ -22,4 +22,18 @@ public class Case {
         this.nom = "";
         this.position = 0;
     }
+
+    public String toString(){
+        String msg = this.nom ;
+        if(this instanceof Achetable){
+            msg = msg + "coût : " + String.valueOf(this.prix) + " - " ;
+            if (this.proprietaire == None){
+                msg = msg + "sans propriétaire";
+            }
+            else {
+                msg = msg + "propriétaire : " + this.proprietaire.nom + " - " + "fortune : " + String.valueOf(this.proprietaire.fortune); 
+            }
+        }
+        System.out.println(msg);
+    }
 }
