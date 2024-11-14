b7971f853c57ba3916449df645417bb7bbe5a559
TitouanBrousse <titouan.br@gmail.com>
2024-11-14 11:44:05 +0100

    Update Rue.java

--- jeu/src/main/java/org/centrale/medev/monopoly/Rue.java
+++ jeu/src/main/java/org/centrale/medev/monopoly/Rue.java
@@ -37,18 +37,33 @@ public class Rue extends Achetable{
     }
     
     public void construireMaison(int nbMConstruite){
-        int newNbM = this.getNbM() + nbMConstruite;
-        this.setNbM(newNbM);
+        if (this.proprietaire.getFortune() >= nbMConstruite*150 && nbMConstruite <= 4){
+            int newNbM = this.getNbM() + nbMConstruite;
+            this.setNbM(newNbM);
+            int newFortune = this.proprietaire.getFortune() - nbMConstruite*150;
+            this.proprietaire.setFortune(newFortune);
+            System.out.println(this.proprietaire.getNom()+" a construit "+nbMConstruite+" maison(s).");
+        } else {
+            System.out.println("Le nombre de maisons rentrées n'est pas correct");
+        }
     }
     
-    public void construireHotel(int nbHConstruite){
-        int newNbH = this.getNbH() + nbHConstruite;
-        this.setNbH(newNbH);
+    public void construireHotel(int nbHConstruit){
+        int cout = 1000 - this.nbM*200;
+        if (this.proprietaire.getFortune() >= cout && nbHConstruit == 0){
+            this.setNbH(1);
+            this.setNbM(0);
+            int newFortune = this.proprietaire.getFortune() - cout;
+            this.proprietaire.setFortune(newFortune);
+            System.out.println(this.proprietaire.getNom()+" a construit un hôtel.");
+        } else {
+            System.out.println("Le nombre d'hôtel rentré n'est pas correct");
+        }
     }
     
     public int calculLoyer(){
         int a = 200;
-        int b = 1000;
+        int b = 1200;
         int base = 100;
         int loyer = this.getNbH()*a + this.getNbM()*b;
         if (loyer==0){loyer = base;}
