2125be7ef67e5800a8efd509a974666dc1a0fa33
paulmauny <paulmauny@yahoo.fr>
2024-11-14 11:33:26 +0100

    Create AllerPrison.java

---
+++ jeu/src/main/java/org/centrale/medev/monopoly/AllerPrison.java
@@ -0,0 +1,22 @@
+package org.centrale.medev.monopoly
+
+public class AllerPrison extends Evenement{
+    private Joueur joueur ;
+    
+    public AllerPrison(String nom, int position){
+        super(position,nom) ;
+    }
+    
+    public AllerPrison(){
+        super() ;
+    }
+    
+    public AllerPrison(AllerPrison ap){
+        super(ap) ;
+    }
+    
+    public void effet(){
+        joueur.goToPrison();
+    }
+    
+}
