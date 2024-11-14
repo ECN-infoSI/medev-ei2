cb075f0d4a40ea9eb9c3774331f298e3aaa52b15
truaux <thomas003.ruaux@gmail.com>
2024-11-14 09:45:37 +0100

    update Achetable

--- jeu/src/main/java/org/centrale/medev/monopoly/Achetable.java
+++ jeu/src/main/java/org/centrale/medev/monopoly/Achetable.java
@@ -2,14 +2,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.monopoly;
+package org.centrale.medev.monopoly;
 
 /**
  *
  * @author Thomas
  * 
  */
-public class Gare {
+public class Achetable extends Case {
     
+    private int prix;
+    private Joueur proprietaire;
+
+    public Achetable(){
+        super();
+        prix = 0;
+        proprietaire = null;
+    }
+
+    public Achetable(String n, int pos, int p, Joueur j){
+        super(n, pos);
+        prix = p;
+        proprietaire = j;
+    }
+
+    public int calculLoyer(){
+
+    }
 
 }
