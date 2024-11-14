8544077c459c1d703058eb7747c09602fc75c913
paulmauny <paulmauny@yahoo.fr>
2024-11-14 11:42:06 +0100

    Create Prison.java

---
+++ jeu/src/main/java/org/centrale/medev/monopoly/Prison.java
@@ -0,0 +1,15 @@
+package org.centrale.medev.monopoly ;
+
+public class Prison extends Evenement {
+    public Prison(String nom, int position){
+        super(position,nom) ;
+    }
+    
+    public Prison(){
+        super() ;
+    }
+    
+    public Prison(Prison c){
+        super(c) ;
+    }
+}
