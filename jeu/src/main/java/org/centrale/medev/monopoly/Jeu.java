6b8f97b212884580ec59723ef75b6f631f158558
Ulysse <hansschmidt884@yahoo.com>
2024-11-14 08:27:59 +0100

    Création fichiers initiaux

---
+++ jeu/src/main/java/org/centrale/medev/monopoly/Jeu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev.monopoly;
+
+/**
+ *
+ * @author hanss
+ */
+public class Jeu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
