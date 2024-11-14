3dde099696d4c8329057a03ff746bc1d158b1e72
paulmauny <paulmauny@yahoo.fr>
2024-11-14 11:40:30 +0100

    Update Paiement.java

--- jeu/src/main/java/org/centrale/medev/monopoly/Paiement.java
+++ jeu/src/main/java/org/centrale/medev/monopoly/Paiement.java
@@ -1,18 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.centrale.medev.monopoly;
 
 public class Paiement extends Evenement {
     
     public int prix ;
     
+    public Joueur joueur ;
+    
     public Paiement(String nom, int position, int prix){
         super(position,nom) ;
         this.prix = prix ;
     }
-    
+
     public Paiement(){
         super() ;
         this.prix = 2000 ;
@@ -22,7 +20,11 @@ public class Paiement extends Evenement {
         super(p);
         this.prix = p.prix ;
     }
-
+    
+    public void effet(){
+        joueur.paiement(prix) ;
+    }
+    
     public void setPrix(int prix) {
         this.prix = prix;
     }
